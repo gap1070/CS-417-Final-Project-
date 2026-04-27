9941bded8cde6d5bf529979dd4e810823fc69ce1
gap1070 <gap1070@usnh.edu>
2026-04-27 15:12:51 +0000

    Defined main, connected the Task class to main, and tested the outputs. I deleted the tests so they aren't in the way for tomorrow.

---
+++ your-project/src/your_project/main.py
@@ -0,0 +1,12 @@
+# This file is the main entry for this program 
+
+# Import the Task class from task.py
+from your_project.task import Task 
+
+def main():
+    # Starting Message 
+    print("Smart TO-DO List starting...")
+
+# Runs the program 
+if __name__ == "__main__":
+    main()
