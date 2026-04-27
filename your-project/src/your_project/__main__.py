125af573203791b8ffa489bcce0e378a5a3c1e55
gap1070 <gap1070@usnh.edu>
2026-04-27 15:28:09 +0000

    Made runnable with "python -m your_project"

---
+++ your-project/src/your_project/__main__.py
@@ -0,0 +1,6 @@
+# Imports the main function from main.py
+from .main import main 
+
+# Runs when this is ran as the entry point for the program 
+if __name__ == "__main__":
+    main()
--- your-project/src/your_project/main.py
+++ your-project/src/your_project/main.py
@@ -1,7 +1,7 @@
 # This file is the main entry for this program 
 
 # Import the Task class from task.py
-from your_project.task import Task 
+from .task import Task 
 
 def main():
     # Starting Message 
