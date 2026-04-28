dc82bed7a324a86b70f2910de0145b091993b6e8
gap1070 <gap1070@usnh.edu>
2026-04-28 18:42:07 +0000

    Added example tasks, and the process to make sure the task are in correct order.

--- your-project/src/your_project/main.py
+++ your-project/src/your_project/main.py
@@ -5,8 +5,18 @@ from .task import Task
 from .task_manager import TaskManager
 
 def main():
-    # Starting Message 
-    print("Smart TO-DO List starting...")
+    # Creates manager 
+    manager = TaskManager()
+
+    # Example tasks 
+    manager.add_task(Task("Homework", 5, 4))
+    manager.add_task(Task("Workout", 3, 3))
+    manager.add_task(Task("Videgames", 2, 2))
+
+    # Makes sure to process the tasks in the right order 
+    while not manager.is_empty():
+        task = manager.get_next_task()
+        print(task)
 
 # Runs the program 
 if __name__ == "__main__":
