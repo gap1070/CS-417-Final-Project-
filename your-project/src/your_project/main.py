43b39b7569b7221aeb28a2fedc4807ff7a8e6d57
gap1070 <gap1070@usnh.edu>
2026-04-29 16:24:37 +0000

    Created option 1 for the option menu

--- your-project/src/your_project/main.py
+++ your-project/src/your_project/main.py
@@ -8,15 +8,38 @@ def main():
     # Creates manager 
     manager = TaskManager()
 
-    # Example tasks 
-    manager.add_task(Task("Homework", 5, 4))
-    manager.add_task(Task("Workout", 3, 3))
-    manager.add_task(Task("Video games", 2, 2))
+    # Creates a loop that will go on until user selects exit 
+    while True:
+        # Option Menu
+        print("\n=== Smart To-DO List ===")
+        print("1. Add Task")
+        print("2. Get Next Task")
+        print("3. Exit")
 
-    # Makes sure to process the tasks in the right order 
-    while not manager.is_empty():
-        task = manager.get_next_task()
-        print(task)
+        # Gets the users choice 
+        choice = input("Choose an option: ")
+
+        # Option 1: Adds new task
+        if choice == "1":
+            name = input("Task Name: ")
+
+            # Makes sure numbers are valid 
+            try:
+                importance = int(input("Importance (1-5): "))
+                urgency = int(input("Urgency (1-5): "))
+            except ValueError:
+                print("Please enter valid numbers.")
+                # restarts the loop if the numbers are not valid 
+                continue 
+
+            # Actually creates and add the task
+            task = Task(name, importance, urgency)
+            manager.add_task(task)
+            print("Task Added!")
+
+        # Option 2: Get next highest priority task
+
+
 
 # Runs the program 
 if __name__ == "__main__":
